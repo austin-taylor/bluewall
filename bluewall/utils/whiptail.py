82533cbf9fd584a9d388b4135c5899c5daaaa862
Nick Lupien <nick.lupien@gmail.com>
2017-03-06 22:07:55 -0500

    added whiptail.py to utils

---
+++ bluewall/utils/whiptail.py
@@ -0,0 +1,82 @@
+# whiptail.py - Use whiptail to display dialog boxes from shell scripts
+# Copyright (C) 2013 Marwan Alsabbagh
+# license: BSD, see LICENSE for more details.
+
+from __future__ import print_function
+import sys
+import shlex
+import itertools
+from subprocess import Popen, PIPE
+from collections import namedtuple
+
+__version__ = '0.2'
+PY3 = sys.version_info[0] == 3
+string_types = str if PY3 else basestring
+Response = namedtuple('Response', 'returncode value')
+
+
+def flatten(data):
+    return list(itertools.chain.from_iterable(data))
+
+
+class Whiptail(object):
+    def __init__(self, title='', backtitle='', height=10, width=50,
+                 auto_exit=True):
+        self.title = title
+        self.backtitle = backtitle
+        self.height = height
+        self.width = width
+        self.auto_exit = auto_exit
+
+    def run(self, control, msg, extra=(), exit_on=(1, 255)):
+        cmd = [
+            'whiptail', '--title', self.title, '--backtitle', self.backtitle,
+            '--' + control, msg, str(self.height), str(self.width)
+        ]
+        cmd += list(extra)
+        p = Popen(cmd, stderr=PIPE)
+        out, err = p.communicate()
+        if self.auto_exit and p.returncode in exit_on:
+            print('User cancelled operation.')
+            sys.exit(p.returncode)
+        return Response(p.returncode, err)
+
+    def prompt(self, msg, default='', password=False):
+        control = 'passwordbox' if password else 'inputbox'
+        return self.run(control, msg, [default]).value
+
+    def confirm(self, msg, default='yes'):
+        defaultno = '--defaultno' if default == 'no' else ''
+        return self.run('yesno', msg, [defaultno], [255]).returncode == 0
+
+    def alert(self, msg):
+        self.run('msgbox', msg)
+
+    def view_file(self, path):
+        self.run('textbox', path, ['--scrolltext'])
+
+    def calc_height(self, msg):
+        height_offset = 8 if msg else 7
+        return [str(self.height - height_offset)]
+
+    def menu(self, msg='', items=(), prefix=' - '):
+        if isinstance(items[0], string_types):
+            items = [(i, '') for i in items]
+        else:
+            items = [(k, prefix + v) for k, v in items]
+        extra = self.calc_height(msg) + flatten(items)
+        return self.run('menu', msg, extra).value
+
+    def showlist(self, control, msg, items, prefix):
+        if isinstance(items[0], string_types):
+            items = [(i, '', 'OFF') for i in items]
+        else:
+            items = [(k, prefix + v, s) for k, v, s in items]
+        extra = self.calc_height(msg) + flatten(items)
+        return shlex.split(self.run(control, msg, extra).value)
+
+    def radiolist(self, msg='', items=(), prefix=' - '):
+        return self.showlist('radiolist', msg, items, prefix)
+
+    def checklist(self, msg='', items=(), prefix=' - '):
+        return self.showlist('checklist', msg, items, prefix)
